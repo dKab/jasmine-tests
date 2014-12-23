5081ade4b77b3e0566b28f9aa8c722349e1ce8fb
DmitriyKab <dkabardinov@gmail.com>
2014-12-23 12:06:04 +0500

    add tests to 3 task

---
+++ 3.js
@@ -0,0 +1,34 @@
+describe("Function map(fn, array)", function() {
+	var curtime = new Date().getTime().toString(),
+		rand  = Math.random(),
+		orig, res, tmpObj, remember;
+	var mutate = function(obj) {
+		obj[curtime] = rand;
+		return obj;
+	};
+
+	beforeEach(function() {
+		tmpObj  = {
+			spy: mutate
+		};
+		orig = [{}, {}, {}, {}];
+		remember = orig.slice(0);
+		spyOn(tmpObj, 'spy').and.callThrough();
+		res = map(tmpObj.spy, orig);
+	});
+	
+	it("should return an array", function() {
+		expect(res.constructor).toBe(Array);
+	});
+	it("should apply function passed as 1st arg to each element of array passed as 2nd arg", function() {	
+		expect(tmpObj.spy.calls.count()).toEqual(orig.length);
+		expect(res.length).toEqual(orig.length);
+		for (var i = 0; i < res.length; i++) {
+			expect(res[i][curtime]).toBeDefined();
+			expect(res[i][curtime]).toEqual(rand);
+		}
+	});
+	it("should not change original array", function() {
+		expect(orig).toEqual(remember);
+	});
+});

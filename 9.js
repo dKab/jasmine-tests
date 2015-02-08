9a9a08c781c5b6e7b7d224ad983df931a7a81d5f
codedokode <codedokode@gmail.com>
2015-02-08 03:36:01 +0300

    Changed arguments order for task 9 & fixed HTML markup

--- 9.js
+++ 9.js
@@ -1,15 +1,24 @@
-describe('Функция filter(fn, arr)', function() {
+describe('Функция filter(arr, fn)', function() {
 	it('должна возвращать массив значений для которых fn вернет true', function() {
 		var arr = [0, -1, 5, -7, 6, 100, -10],
-		isNegative = function (val) { return val < 0; },
-		res = filter(isNegative, arr);
+			isNegative = function (val) { return val < 0; },
+			res = filter(arr, isNegative);
+
 		expect(res).toEqual([-1, -7, -10]);
 	});
 	it('не должна изменять исходный массив', function() {
 		var arr = ['foo', 'bar', 'Baz', 'quax'],
 			copy = arr.slice(),
 			startsWithB = function(str) { return str.indexOf('b') === 0; };
-			filter(startsWithB, arr);
-			expect(arr).toEqual(copy);
+
+		filter(arr, startsWithB);
+		expect(arr).toEqual(copy);
 	});
+	it('при передаче пустого массива должна возвращать пустой массив', function () {
+		function falseFn() { 
+			return false; 
+		}
+		
+		expect(filter([], falseFn)).toEqual([]);
+	})
 });

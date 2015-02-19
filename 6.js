e7f7dbe9bb4280ab8b5baefff90526b6ba77a3fb
codedokode <codedokode@gmail.com>
2015-02-19 15:03:18 +0300

    More detailed output for task 6

--- 6.js
+++ 6.js
@@ -5,24 +5,33 @@ describe("Функция partialAny(fn, foo, bar...)", function() {
 		return {a: a, b: b, c:c, d:d, e:e, f:f};
 	}
 	var _test2 = partialAny(test2, undefined, 1, undefined, 2, undefined, 4);
-	it("должна испольльзовать строгое сравнение с undefined", function() {
-		var testNull = partialAny(test2, undefined, 1, null, 2, undefined, 4);
-		var res = testNull(5, 10, 15);
-		expect(res.c).toBe(null);
-	});
+
 	it("дожна возвращать функцию", function() {
 		expect(test5_10).toEqual(jasmine.any(Function));
 	});
+	
+	it("должна использовать строгое сравнение с undefined (null или 0 не должен восприниматься как undefined)", function() {
+		var testNull = partialAny(test2, 0, 1, null, 2, undefined, 4);
+		var res = testNull(5, 10, 15);
+		expect(res.a).toBe(0);
+		expect(res.c).toBe(null);
+	});
+
 	describe("Возвращаемая функция", function() {
-		it("должна вызывать функцию fn с аргументами foo, bar и т.д.", function() {
-			var res = test5_10(7),
-			 res2 = test5_10(),
-			  res3 = _test2(),
-			  res4  = _test2(1, 3, 9);
-			expect(res.b).toEqual(7);
-			expect(res2).toEqual({a: 5, b: undefined, c: 10});
-			expect(res3).toEqual({a: undefined, b: 1, c:undefined, d: 2, e: undefined, f: 4});
-			expect(res4).toEqual({a: 1, b: 1, c: 3, d:2, e:9, f: 4});
+		it("должна подставлять переданный аргумент в partialAny(fn, 5, undefined, 10)", function () {
+			expect(test5_10(7).b).toEqual(7);
+		});
+
+		it("должна подставлять undefined если аргумент не передан в partialAny(fn, 5, undefined, 10)", function () {
+			expect(test5_10()).toEqual({a: 5, b: undefined, c: 10});
+		});
+
+		it("должна подставлять несколько аргументов в partialAny(fn, a, undefined, undefined, b)", function () {
+			expect(_test2(7, 3, 9)).toEqual({a: 7, b: 1, c: 3, d:2, e:9, f: 4});
+		});
+
+		it("должна подставлять undefined если аргумент не передан в partialAny(fn, a, undefined, undefined, b)", function () {
+			expect(_test2(7)).toEqual({a: 7, b: 1, c: undefined, d:2, e:undefined, f: 4});
 		});
 	});
 });
